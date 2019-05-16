df39d3f27408f31c9f3deb4eb2ba84ab1219cec0
Kgupi <Kgupi@users.noreply.github.com>
2019-05-16 05:47:30 +0200

    Added GymAPI.ViewModels project classes.

---
+++ GymAPI.ViewModels/MemberRequest.cs
@@ -0,0 +1,18 @@
+﻿namespace GymAPI.ViewModels
+{
+    public class MemberRequest
+    {
+        public string MemberName { get; set; }
+    }
+
+    public class MemberResponse
+    {
+        public string MemberNo { get; set; }
+        public string MemberName { get; set; }
+    }
+
+    public class MemberNoRequest
+    {
+        public string MemberNo { get; set; }
+    }
+}
---
+++ GymAPI.ViewModels/YearWiseRequestModel.cs
@@ -0,0 +1,8 @@
+﻿namespace GymAPI.ViewModels
+{
+    public class YearWiseRequestModel
+    {
+        // ReSharper disable once InconsistentNaming
+        public string YearID { get; set; }
+    }
+}
